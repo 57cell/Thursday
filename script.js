ba13c5c6037452c2dc13bf4f8c456d7f00ef745e
57cell <100093400+57cell@users.noreply.github.com>
2023-08-20 03:56:28 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const darkModeToggle = document.getElementById('dark-mode-toggle');
+    const animatedSection = document.querySelector('.animated-section');
+
+    let darkMode = false;
+
+    darkModeToggle.addEventListener('click', function () {
+        darkMode = !darkMode;
+        if (darkMode) {
+            document.body.classList.add('dark-mode');
+        } else {
+            document.body.classList.remove('dark-mode');
+        }
+    });
+
+    window.addEventListener('scroll', function () {
+        const sectionPos = animatedSection.getBoundingClientRect().top;
+        const windowHeight = window.innerHeight;
+
+        if (sectionPos < windowHeight) {
+            animatedSection.classList.add('visible');
+        }
+    });
+});
 
