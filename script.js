9cd5c467689920997d557bde09e3ccd80b595a2e
57cell <100093400+57cell@users.noreply.github.com>
2023-08-20 03:39:59 +0000

    Update script.js

--- script.js
+++ script.js
@@ -7,6 +7,56 @@ document.querySelectorAll('a').forEach(anchor => {
     });
 });
 
+window.addEventListener('scroll', handleScrollAnimation);
+window.addEventListener('scroll', updateActiveSection);
+
+// Number of bubbles you want to create
+const numberOfBubbles = 10;
+
+// Create bubble elements and append to the body
+for (let i = 0; i < numberOfBubbles; i++) {
+    const bubble = document.createElement('div');
+    bubble.className = 'bubble';
+    document.body.appendChild(bubble);
+    const size = Math.floor(Math.random() * 120) + 80;
+    bubble.style.width = `${size}px`;
+    bubble.style.height = `${size}px`;
+    positionBubble(bubble);
+    animateBubble(bubble);
+}
+
+function positionBubble(bubble) {
+    bubble.style.top = `${Math.floor(Math.random() * window.innerHeight)}px`;
+    bubble.style.left = `${Math.floor(Math.random() * window.innerWidth)}px`;
+}
+
+let scrollSpeed = 0;
+
+function animateBubble(bubble) {
+    let x = parseFloat(bubble.style.left);
+    let y = parseFloat(bubble.style.top);
+    const step = () => {
+        x += (Math.random() * 2 - 1 + scrollSpeed * 0.1);
+        y += (Math.random() * 2 - 1 + scrollSpeed * 0.1);
+
+        bubble.style.left = `${(x + window.innerWidth) % window.innerWidth}px`;
+        bubble.style.top = `${(y + window.innerHeight) % window.innerHeight}px`;
+
+        requestAnimationFrame(step);
+    };
+
+    step();
+}
+
+let lastScrollY = window.scrollY;
+window.addEventListener('scroll', () => {
+    scrollSpeed = window.scrollY - lastScrollY;
+    lastScrollY = window.scrollY;
+
+    setTimeout(() => {
+        scrollSpeed *= 0.9;
+    }, 100);
+});
 function handleScrollAnimation() {
     const elements = document.querySelectorAll('.fade-in-up');
     const windowHeight = window.innerHeight;
@@ -19,8 +69,6 @@ function handleScrollAnimation() {
         }
     });
 }
-
-window.addEventListener('scroll', handleScrollAnimation);
 
 function updateActiveSection() {
     const sections = document.querySelectorAll('section');
@@ -43,66 +91,3 @@ function updateActiveSection() {
         }
     });
 }
-
-window.addEventListener('scroll', updateActiveSection);
-
-// Number of bubbles you want to create
-const numberOfBubbles = 10;
-
-// Create bubble elements and append to the body
-for (let i = 0; i < numberOfBubbles; i++) {
-    const bubble = document.createElement('div');
-    bubble.className = 'bubble';
-    document.body.appendChild(bubble);
-    const size = Math.floor(Math.random() * 120) + 80;
-    bubble.style.width = `${size}px`;
-    bubble.style.height = `${size}px`;
-    positionBubble(bubble);
-    animateBubble(bubble);
-}
-
-
-// Apply styling to the bubbles
-const bubbles = document.querySelectorAll('.bubble');
-bubbles.forEach((bubble) => {
-    const size = Math.floor(Math.random() * 120) + 80;
-    bubble.style.width = `${size}px`;
-    bubble.style.height = `${size}px`;
-    bubble.style.borderRadius = '50%';
-    bubble.style.position = 'absolute';
-    bubble.style.background = '#4a0072';
-    bubble.style.opacity = Math.random();
-});
-
-function positionBubble(bubble) {
-    bubble.style.top = `${Math.floor(Math.random() * window.innerHeight)}px`;
-    bubble.style.left = `${Math.floor(Math.random() * window.innerWidth)}px`;
-}
-
-let scrollSpeed = 0;
-
-function animateBubble(bubble) {
-    const animate = () => {
-        const x = parseFloat(bubble.style.left);
-        const y = parseFloat(bubble.style.top);
-        const newX = x + (Math.random() * 2 - 1 + scrollSpeed * 0.1);
-        const newY = y + (Math.random() * 2 - 1 + scrollSpeed * 0.1);
-
-        bubble.style.left = `${(newX + window.innerWidth) % window.innerWidth}px`;
-        bubble.style.top = `${(newY + window.innerHeight) % window.innerHeight}px`;
-
-        requestAnimationFrame(animate);
-    };
-
-    animate();
-}
-
-let lastScrollY = window.scrollY;
-window.addEventListener('scroll', () => {
-    scrollSpeed = window.scrollY - lastScrollY;
-    lastScrollY = window.scrollY;
-
-    setTimeout(() => {
-        scrollSpeed *= 0.9;
-    }, 100);
-});
