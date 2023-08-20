e44b4c19bd570e98246b96b803ac86dacef96570
57cell <100093400+57cell@users.noreply.github.com>
2023-08-20 03:26:35 +0000

    Update script.js

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ function updateActiveSection() {
     sections.forEach((section, index) => {
         const sectionTop = section.getBoundingClientRect().top;
 
-        if (sectionTop <= 100) { // 100px offset to handle the sticky header
+        if (sectionTop <= 100) {
             currentSection = index;
         }
     });
@@ -46,47 +46,44 @@ function updateActiveSection() {
 
 window.addEventListener('scroll', updateActiveSection);
 
-// Code for creating floating bubbles
+// Number of bubbles you want to create
 const numberOfBubbles = 10;
+
 // Create bubble elements and append to the body
 for (let i = 0; i < numberOfBubbles; i++) {
     const bubble = document.createElement('div');
     bubble.className = 'bubble';
     document.body.appendChild(bubble);
-    positionBubble(bubble); // Function to set initial position
-    animateBubble(bubble); // Function to animate bubble
+    positionBubble(bubble);
+    animateBubble(bubble);
 }
 
 // Apply styling to the bubbles
 const bubbles = document.querySelectorAll('.bubble');
 bubbles.forEach((bubble) => {
-    const size = Math.floor(Math.random() * 120) + 80; // Larger and more variable sizes
+    const size = Math.floor(Math.random() * 120) + 80;
     bubble.style.width = `${size}px`;
     bubble.style.height = `${size}px`;
     bubble.style.borderRadius = '50%';
     bubble.style.position = 'absolute';
-    bubble.style.background = '#4a0072'; // Deep purple color
-    bubble.style.opacity = Math.random(); // Random opacity for more depth
+    bubble.style.background = '#4a0072';
+    bubble.style.opacity = Math.random();
 });
 
-// Function to set a random initial position for a bubble
 function positionBubble(bubble) {
     bubble.style.top = `${Math.floor(Math.random() * window.innerHeight)}px`;
     bubble.style.left = `${Math.floor(Math.random() * window.innerWidth)}px`;
 }
 
-let scrollSpeed = 0; // Variable to hold scroll speed
+let scrollSpeed = 0;
 
-// Function to animate a bubble
 function animateBubble(bubble) {
-    // Use requestAnimationFrame for smooth animation
     const animate = () => {
         const x = parseFloat(bubble.style.left);
         const y = parseFloat(bubble.style.top);
         const newX = x + (Math.random() * 2 - 1 + scrollSpeed * 0.1);
         const newY = y + (Math.random() * 2 - 1 + scrollSpeed * 0.1);
 
-        // Wrap around the screen if the bubble goes out of bounds
         bubble.style.left = `${(newX + window.innerWidth) % window.innerWidth}px`;
         bubble.style.top = `${(newY + window.innerHeight) % window.innerHeight}px`;
 
@@ -96,13 +93,11 @@ function animateBubble(bubble) {
     animate();
 }
 
-// Event listener to detect scrolling speed
 let lastScrollY = window.scrollY;
 window.addEventListener('scroll', () => {
     scrollSpeed = window.scrollY - lastScrollY;
     lastScrollY = window.scrollY;
 
-    // Gradually reduce scrollSpeed back to 0 when not scrolling
     setTimeout(() => {
         scrollSpeed *= 0.9;
     }, 100);
