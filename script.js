774ed45cbc1cd8a6755f37550d45a5e8986b7664
57cell <100093400+57cell@users.noreply.github.com>
2023-08-25 02:56:30 +0000

    Update script.js

--- script.js
+++ script.js
@@ -3,60 +3,55 @@ document.addEventListener('DOMContentLoaded', function () {
   let currentMonth = new Date().getMonth();
   let currentYear = new Date().getFullYear();
 
-function createCalendar() {
+  function createCalendar() {
     const today = new Date();
     const month = today.getMonth();
     const year = today.getFullYear();
-    
-    // Get the current month's name
-    const monthName = new Intl.DateTimeFormat('en-US', { month: 'long' }).format(today);
-    
-    document.getElementById('monthYear').innerText = `${monthName} ${year}`;
-    
+
     // Start building the calendar
     let calendarBody = '';
-    
+
     // Get the first day of the month
     let firstDay = new Date(year, month, 1);
 
     // Fill in the blank spaces before the first day
     for (let i = 0; i < firstDay.getDay(); i++) {
-        calendarBody += '<td></td>';
+      calendarBody += '<td></td>';
     }
-    
-  // Fill in the days of the month
-  for (let i = 1; i <= 31; i++) {
+
+    // Fill in the days of the month
+    for (let i = 1; i <= 31; i++) {
       const dayDate = new Date(year, month, i);
       if (dayDate.getMonth() !== month) break; // Handle months with less than 31 days
-      
+
       // Check if today
       if (i === today.getDate() && month === today.getMonth() && year === today.getFullYear()) {
-          calendarBody += `<td id="day-${year}-${month + 1}-${i}" class="current-day">${i}</td>`;
+        calendarBody += `<td id="day-${year}-${month + 1}-${i}" class="current-day">${i}</td>`;
       } else {
-          calendarBody += `<td id="day-${year}-${month + 1}-${i}">${i}</td>`;
+        calendarBody += `<td id="day-${year}-${month + 1}-${i}">${i}</td>`;
       }
-      
+
       // Check if the week is complete
       if (dayDate.getDay() === 6) {
-          calendarBody += '</tr><tr>';
+        calendarBody += '</tr><tr>';
       }
-  }
-    
+    }
+
     // Add the generated body to the calendar
     document.querySelector('.calendar tbody').innerHTML += `<tr>${calendarBody}</tr>`;
-}
+  }
 
-// Execute the function to create the calendar
-createCalendar();
-});
+  // Execute the function to create the calendar
+  createCalendar();
 
-// Get the current date
-var today = now.getDate();
+  // Get the current date
+  var today = new Date().getDate();
 
-// Find the table cell that corresponds to today's date
-var calendarCells = document.querySelectorAll(".calendar td");
-calendarCells.forEach(function(cell) {
+  // Find the table cell that corresponds to today's date
+  var calendarCells = document.querySelectorAll(".calendar td");
+  calendarCells.forEach(function (cell) {
     if (parseInt(cell.innerText) === today) {
-        cell.classList.add("today");
+      cell.classList.add("today");
     }
+  });
 });
