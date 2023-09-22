2d43727523cd38de5e4be439b13bdb617f2bb4b7
57cell <100093400+57cell@users.noreply.github.com>
2023-09-22 11:51:55 +0000

    Update script.js

--- contact/script.js
+++ contact/script.js
@@ -1,8 +1,18 @@
+let currentOpenDesc = null;
+
 function toggleDescription(descId) {
     let description = document.getElementById(descId);
+    if (window.innerWidth > 768) {
+        return;
+    }
+    if (currentOpenDesc && currentOpenDesc !== description) {
+        currentOpenDesc.style.display = "none";
+    }
     if(description.style.display === "block") {
         description.style.display = "none";
+        currentOpenDesc = null;
     } else {
         description.style.display = "block";
+        currentOpenDesc = description;
     }
 }
